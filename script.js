29f6ffd82fbfe2b88af2af24069d705a8a37084a
Zhiyuan Liu <68548366+ZealinBee@users.noreply.github.com>
2022-12-04 22:26:59 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+document.addEventListener("DOMContentLoaded", loadLocalStorage)
+
 //Imports
 import numberOfDaysInAMonth from "./daysCount.js";
+
 
 //Editable variables
 let totalSpentMoney = 0;
@@ -7,50 +10,6 @@ let monthlyAllowance = 0;
 let dailyAllowance = 0;
 let totalDaysUsed = 0;
 let currentDay = 0
-
-//Initialize calendar
-var calendarEl = document.getElementById("calendar");
-var calendar = new FullCalendar.Calendar(calendarEl, {
-  initialView: "dayGridMonth",
-});
-calendar.render();
-
-//Functions
-const submitMonthlyMoneyHandler = () => {
-  monthlyAllowanceContainer.classList.toggle("hide");
-  monthlyAllowance = monthlyAllowanceInput.value - monthlySaveInput.value;
-  localStorage.setItem("local-monthly-allowance", monthlyAllowance);
-  totalMonthlyAllowanceSpan.textContent = monthlyAllowance;
-  dailyAllowance = (monthlyAllowance / numberOfDaysInAMonth).toFixed(2);
-  dailyAllowanceSpan.textContent = dailyAllowance;
-};
-
-const submitSpentMoneyHandler = () => {
-  totalSpentMoney += Number(spentMoneyInput.value);
-  spentMoneyInput.value = "";
-  totalSpentMoneySpan.textContent = totalSpentMoney;
-  totalDaysUsed = totalSpentMoney / dailyAllowance;
-  totalDaysUsedSpan.textContent = totalDaysUsed.toFixed(0);
-  if (totalDaysUsed > numberOfDaysInAMonth) {
-    warning.textContent = `Bruh, you have exceeded the monthly allowance by ${(
-      totalDaysUsed - numberOfDaysInAMonth
-    ).toFixed(0)} day(s)`;
-  }
-  console.group(totalDaysUsed.toFixed(0))
-  for (let i = 0; i < totalDaysUsed.toFixed(0); i++) {
-    currentDay += 1;
-    if(currentDay < 10) {
-      currentDay = `0${currentDay}`
-    }
-    calendar.addEvent({
-      title: 'day used!', 
-      start: `2022-12-${currentDay}`, 
-    });
-    currentDay = i + 1;
-  }
-};
-
-
 
 //Month set money part
 const monthlyAllowanceInput = document.querySelector(
@@ -64,18 +23,79 @@ const totalMonthlyAllowanceSpan = document.querySelector(
   "#total-monthly-allowance-span"
 );
 const submitMonthlyMoney = document.querySelector("#submit-monthly-money");
-const calendarDiv = document.querySelector("#calendar");
 const submitSpentMoney = document.querySelector("#submit-spent-money");
 const spentMoneyInput = document.querySelector("#spent-money-input");
 const totalSpentMoneySpan = document.querySelector("#total-spent-money-span");
 const dailyAllowanceSpan = document.querySelector("#daily-allowance-span");
 const totalDaysUsedSpan = document.querySelector("#total-days-used-span");
 const warning = document.querySelector("#warning");
+const resetButton = document.querySelector('.reset-button')
+
+//Initialize calendar
+var calendarEl = document.getElementById("calendar");
+var calendar = new FullCalendar.Calendar(calendarEl, {
+  initialView: "dayGridMonth",
+});
+calendar.render();
+
+
+//Functions
+const submitMonthlyMoneyHandler = () => {
+  monthlyAllowanceContainer.classList.toggle("hide");
+  monthlyAllowance = monthlyAllowanceInput.value - monthlySaveInput.value;
+  localStorage.setItem("local-monthly-allowance", monthlyAllowance);
+  totalMonthlyAllowanceSpan.textContent = localStorage.getItem("local-monthly-allowance");
+  dailyAllowance = (monthlyAllowance / numberOfDaysInAMonth).toFixed(2);
+  localStorage.setItem("local-daily-allowance", dailyAllowance)
+  dailyAllowanceSpan.textContent = localStorage.getItem("local-daily-allowance");
+};
+
+const submitSpentMoneyHandler = () => {
+  totalSpentMoney += Number(spentMoneyInput.value);
+  spentMoneyInput.value = "";
+  localStorage.setItem("local-total-spent-money", totalSpentMoney)
+  totalSpentMoneySpan.textContent = totalSpentMoney;
+  totalDaysUsed = totalSpentMoney / dailyAllowance;
+  localStorage.setItem('local-total-days-used', totalDaysUsed)
+  totalDaysUsedSpan.textContent = totalDaysUsed.toFixed(0);
+  if (totalDaysUsed > numberOfDaysInAMonth) {
+    warning.textContent = `Bruh, you have exceeded the monthly allowance by ${(
+      totalDaysUsed - numberOfDaysInAMonth
+    ).toFixed(0)} day(s)`;
+  }
+  calendar.removeAllEvents()
+
+  for (let i = 0; i < totalDaysUsed.toFixed(0); i++) {
+    currentDay += 1;
+    if(currentDay < 10) {
+      currentDay = `0${currentDay}`
+    }
+    calendar.addEvent({
+      title: 'day used!', 
+      start: `2022-12-${currentDay}`, 
+    });
+    currentDay = i + 1;
+  }
+  
+};
+
+function loadLocalStorage() {
+  monthlyAllowance = localStorage.getItem("local-monthly-allowance")
+  totalMonthlyAllowanceSpan.textContent = monthlyAllowance;
+  dailyAllowance = localStorage.getItem("local-daily-allowance")
+  dailyAllowanceSpan.textContent = dailyAllowance;
+  totalSpentMoney = localStorage.getItem('local-total-spent-money')
+  totalSpentMoneySpan.textContent= totalSpentMoney
+  totalDaysUsed = localStorage.getItem('local-total-days-used')
+  totalDaysUsedSpan.textContent = totalDaysUsed.toFixed(0)
+}
+
+const resetStorage = () => {
+  localStorage.clear()
+  loadLocalStorage()
+}
 
 //Events
 submitMonthlyMoney.addEventListener("click", submitMonthlyMoneyHandler);
 submitSpentMoney.addEventListener("click", submitSpentMoneyHandler);
-
-calendar.on("dateClick", function (info) {
-  console.log("clicked on " + info.dateStr);
-});
+resetButton.addEventListener('click', resetStorage)
