7f0fddbf59786709abc868fe73ea7ac7f7328e70
ZealinBee <theukezealot@gmail.com>
2022-12-17 01:14:21 +0200

    removed local host

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-document.addEventListener("DOMContentLoaded", loadLocalStorage)
 
 //Imports
 import numberOfDaysInAMonth from "./daysCount.js";
@@ -43,21 +42,17 @@ calendar.render();
 const submitMonthlyMoneyHandler = () => {
   monthlyAllowanceContainer.classList.toggle("hide");
   monthlyAllowance = monthlyAllowanceInput.value - monthlySaveInput.value;
-  localStorage.setItem("local-monthly-allowance", monthlyAllowance);
-  totalMonthlyAllowanceSpan.textContent = localStorage.getItem("local-monthly-allowance");
+  totalMonthlyAllowanceSpan.textContent = monthlyAllowance
   dailyAllowance = (monthlyAllowance / numberOfDaysInAMonth).toFixed(2);
-  localStorage.setItem("local-daily-allowance", dailyAllowance)
-  dailyAllowanceSpan.textContent = localStorage.getItem("local-daily-allowance");
+  dailyAllowanceSpan.textContent = dailyAllowance
 };
 
 const submitSpentMoneyHandler = () => {
   parseInt(totalSpentMoney)
   totalSpentMoney += Number(spentMoneyInput.value);
   spentMoneyInput.value = "";
-  localStorage.setItem("local-total-spent-money", totalSpentMoney)
   totalSpentMoneySpan.textContent = totalSpentMoney;
   totalDaysUsed = totalSpentMoney / dailyAllowance;
-  localStorage.setItem('local-total-days-used', totalDaysUsed)
   totalDaysUsedSpan.textContent = totalDaysUsed.toFixed(0);
   if (totalDaysUsed > numberOfDaysInAMonth) {
     warning.textContent = `Bruh, you have exceeded the monthly allowance by ${(
@@ -79,34 +74,8 @@ const submitSpentMoneyHandler = () => {
     currentDay = i + 1;
   }
 
-  localStorage.setItem("local-calendar", calendar)
-
 };
-
-function loadLocalStorage() {
-  monthlyAllowance = localStorage.getItem("local-monthly-allowance")
-  monthlyAllowance = parseInt(monthlyAllowance)
-
-  totalMonthlyAllowanceSpan.textContent = monthlyAllowance;
-  dailyAllowance = localStorage.getItem("local-daily-allowance")
-  dailyAllowance = parseInt(dailyAllowance)
-  dailyAllowanceSpan.textContent = dailyAllowance;
-  totalSpentMoney = localStorage.getItem('local-total-spent-money')
-  totalDaysUsed = parseInt(totalSpentMoney)
-  totalSpentMoneySpan.textContent = totalSpentMoney
-  totalDaysUsed = localStorage.getItem('local-total-days-used')
-  totalDaysUsed = parseInt(totalDaysUsed)
-
-  totalDaysUsedSpan.textContent = totalDaysUsed.toFixed(0)
-
-}
-
-const resetStorage = () => {
-  localStorage.clear()
-  loadLocalStorage()
-}
 
 //Events
 submitMonthlyMoney.addEventListener("click", submitMonthlyMoneyHandler);
 submitSpentMoney.addEventListener("click", submitSpentMoneyHandler);
-resetButton.addEventListener('click', resetStorage)
