5ce0074d922ffdc2396cbdb062547201b40e290b
ZealinBee <theukezealot@gmail.com>
2022-12-17 14:49:46 +0200

    signup and login function added in the frontend

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-
 //Imports
 import numberOfDaysInAMonth from "./daysCount.js";
-
 
 //Editable variables
 let totalSpentMoney = 0;
@@ -9,6 +7,18 @@ let monthlyAllowance = 0;
 let dailyAllowance = 0;
 let totalDaysUsed = 0;
 let currentDay = 0;
+
+//Login sign up part
+const goToSignUpLink = document.querySelector("#go-to-sign-up-link");
+const loginSection = document.querySelector(".login-section");
+const signUpSection = document.querySelector(".signup-section");
+const goToLoginLink = document.querySelector("#go-to-login-link");
+const toggleLogin = document.querySelector(".toggle-login");
+const signUpButton = document.querySelector("#sign-up-button");
+const signUpUserNameInput = document.querySelector("#sign-up-username-input");
+const signUpPasswordInput = document.querySelector(
+  "#sign-up-password-input"
+);
 
 //Month set money part
 const monthlyAllowanceInput = document.querySelector(
@@ -28,7 +38,7 @@ const totalSpentMoneySpan = document.querySelector("#total-spent-money-span");
 const dailyAllowanceSpan = document.querySelector("#daily-allowance-span");
 const totalDaysUsedSpan = document.querySelector("#total-days-used-span");
 const warning = document.querySelector("#warning");
-const resetButton = document.querySelector('.reset-button')
+const resetButton = document.querySelector(".reset-button");
 
 //Initialize calendar
 var calendarEl = document.getElementById("calendar");
@@ -37,18 +47,17 @@ var calendar = new FullCalendar.Calendar(calendarEl, {
 });
 calendar.render();
 
-
 //Functions
 const submitMonthlyMoneyHandler = () => {
   monthlyAllowanceContainer.classList.toggle("hide");
   monthlyAllowance = monthlyAllowanceInput.value - monthlySaveInput.value;
-  totalMonthlyAllowanceSpan.textContent = monthlyAllowance
+  totalMonthlyAllowanceSpan.textContent = monthlyAllowance;
   dailyAllowance = (monthlyAllowance / numberOfDaysInAMonth).toFixed(2);
-  dailyAllowanceSpan.textContent = dailyAllowance
+  dailyAllowanceSpan.textContent = dailyAllowance;
 };
 
 const submitSpentMoneyHandler = () => {
-  parseInt(totalSpentMoney)
+  parseInt(totalSpentMoney);
   totalSpentMoney += Number(spentMoneyInput.value);
   spentMoneyInput.value = "";
   totalSpentMoneySpan.textContent = totalSpentMoney;
@@ -56,26 +65,54 @@ const submitSpentMoneyHandler = () => {
   totalDaysUsedSpan.textContent = totalDaysUsed.toFixed(0);
   if (totalDaysUsed > numberOfDaysInAMonth) {
     warning.textContent = `Bruh, you have exceeded the monthly allowance by ${(
-      totalDaysUsed - numberOfDaysInAMonth 
+      totalDaysUsed - numberOfDaysInAMonth
     ).toFixed(0)} day(s)`;
   }
-  calendar.removeAllEvents()
+  calendar.removeAllEvents();
   currentDay = 0;
   for (let i = 0; i < totalDaysUsed.toFixed(0); i++) {
     currentDay += 21;
     if (currentDay < 10) {
-      currentDay = `0${currentDay}`
+      currentDay = `0${currentDay}`;
     }
     calendar.addEvent({
-      title: 'day used!',
+      title: "day used!",
       start: `2022-12-${currentDay}`,
       end: `2022-12-${currentDay}`,
     });
     currentDay = i + 1;
   }
-
 };
 
 //Events
 submitMonthlyMoney.addEventListener("click", submitMonthlyMoneyHandler);
 submitSpentMoney.addEventListener("click", submitSpentMoneyHandler);
+goToSignUpLink.addEventListener("click", function (e) {
+  loginSection.classList.toggle("hide");
+  signUpSection.classList.toggle("show");
+});
+goToLoginLink.addEventListener("click", function (e) {
+  loginSection.classList.toggle("hide");
+  signUpSection.classList.toggle("show");
+});
+toggleLogin.addEventListener("click", function (e) {
+  loginSection.classList.toggle("hide");
+});
+signUpButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  let userNameInputValue = signUpUserNameInput.value;
+  let passwordInputValue = signUpPasswordInput.value;
+  console.log(userNameInputValue)
+  console.log(passwordInputValue)
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", "/register");
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.send(
+    JSON.stringify({
+      password: passwordInputValue,
+      username: userNameInputValue,
+      totalSpent: 0,
+      totalHave: 0,
+    })
+  );
+});
