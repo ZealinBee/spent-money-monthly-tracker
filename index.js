5541be9e4080f418ee43d34eb832d3c219e32a2d
Roman Zinkevich <roman.zinkevich@tuni.fi>
2022-12-17 21:48:18 +0200

    server

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 
-const port = 3000;
+const port = process.env.PORT || 3000;;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}.`);
