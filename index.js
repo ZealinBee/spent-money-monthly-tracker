c21341e1eaab938d997ae9ebfb8fcf8de4f500d8
RomaZinkevich <zra1903@mail.ru>
2022-12-18 15:33:09 +0200

    .env finished

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 
-const port = process.env.PORT || 3000;;
+const port = process.env.PORT || 5000;;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}.`);
