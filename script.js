4c74418aec01afdebba3b8e8809c162e69184c5c
RomaZinkevich <zra1903@mail.ru>
2023-01-19 21:55:58 +0200

    fixed for server

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Imports
-import numberOfDaysInAMonth from "https://spent-money-monthly-tracker-production.up.railway.app/main.js";
+import numberOfDaysInAMonth from "https://spent-money-monthly-tracker-production.up.railway.app/daysCount.js";
 
 // window.addEventListener("beforeunload", function (e) {
 //   e.preventDefault();
